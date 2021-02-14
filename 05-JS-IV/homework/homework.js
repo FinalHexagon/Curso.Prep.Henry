0b8e698eb6e4b5e2004a19b16c13081c411f4862
Marcos Rios <marcosrioos@gmail.com>
2021-02-14 16:37:22 -0300

    nothing

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,10 +6,10 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-  var Gato={
+  var Gato = {
     nombre: nombre,
-    edad: edad,  
-    meow: function () {
+    edad: edad,
+    meow: function(){
       return "Meow!"
     }
   };
@@ -95,7 +95,7 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
-  if (usuario["password"] === password){
+  if (usuario["password"] === password) {
     return true;
   } else {
     return false;
